e80664bdd063fa63928d51a9a751f96901a434f5
Дерябин Вадим <vadimx66@gmail.com>
2025-10-14 19:05:25 +0500

    Delete TG_bot_2 directory

--- TG_bot_2/ExampleBotCode/src/main/java/ru/eptbw/bot/commands/Command.java
+++
@@ -1,8 +0,0 @@
-package ru.eptbw.bot.commands;
-import org.telegram.telegrambots.meta.api.objects.Update;
-
-public interface Command {
-    String getName();
-    String getDescription();
-    String execute(Update update, String[] args);
-}
